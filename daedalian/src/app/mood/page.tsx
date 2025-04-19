bed77d1086ca3b37dfa1b61f4154db059bf85ff0
BryceeL <bcllamc280@gmail.com>
2025-04-19 07:38:22 -0700

    used mood button component

--- daedalian/src/app/mood/page.tsx
+++ daedalian/src/app/mood/page.tsx
@@ -1,9 +1,30 @@
+'use client'
+ 
+import MoodButton from "../components/MoodButton";
+
+import "./mood.css"; 
+
 export default function Mood() {
-    return (
-      <div>
-        <h1>Mood Tracker</h1>
-        <p>Welcome to the mood tracker.</p>
-      </div>
-    )
+
+  //TODO: Sends a numeric value that reps mood to the page for the AI
+  function ok(){
+    console.log("hi")
   }
+
+  return (
+    <div className='mood-page'>
+      <h1>Mood Tracker</h1>
+      <p>Welcome to the mood tracker.</p>
+
+      
+      <div className="mood-btn-container">
+        <MoodButton
+          moodText="Good"
+          moodValue={1}
+        ></MoodButton>
+      </div>
+      
+    </div>
+  )
+}
   
