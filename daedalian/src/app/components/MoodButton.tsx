98a685dac972f3a052d51624d14170c22700cbf7
BryceeL <bcllamc280@gmail.com>
2025-04-19 07:53:40 -0700

    router push to temporary subdomain and pass params
    The subdomain should be updated to the page with Yong's ai

--- daedalian/src/app/components/MoodButton.tsx
+++ daedalian/src/app/components/MoodButton.tsx
@@ -7,12 +7,14 @@ type moodProps = {
     moodValue: number
 }
 
+//TODO: Change subdomain name of ai to yong's ai page
+
 function MoodButton(props: moodProps) {
     const {moodText, moodValue} = props
     const router = useRouter()
     return (
         <button
-            onClick={() => router.push('/todo')}
+            onClick={() => router.push(`/ai/?mood=${moodValue}`)}
         >{moodText}</button>
     )
 }
